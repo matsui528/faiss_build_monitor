3ace52a86ad6f1ca23d5e812f6fe0ce3cf195970
Yusuke Matsui <matsui528@gmail.com>
2023-05-26 14:44:48 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import faiss
-print("faiss version:", faiss.__version__)
+print("===== faiss version =====")
+print(faiss.__version__)
 
 import numpy
-print("numpy config:")
+print("===== numpy config =====")
 numpy.__config__.show()
+
+
+print("===== Run simple kmeans =====")
+err = faiss.Kmeans(10, 20).train(numpy.random.rand(1000, 10).astype('float32'))
+print(err)
